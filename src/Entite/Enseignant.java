6c02097d259c3fbccbbe0341bcffbbfe82eb7125
Mnaffeti <mohamedaziz.naffeti@esprit.tn>
2024-02-02 11:00:42 +0100

    Adding EnseignantService (not tested)

--- src/Entite/Enseignant.java
+++ src/Entite/Enseignant.java
@@ -1,5 +1,30 @@
 package Entite;
 
 public class Enseignant extends Utilisateur{
-    
+    public Enseignant() {
+    }
+
+    public Enseignant(int idUtilisateur) {
+        super(idUtilisateur);
+    }
+
+    public Enseignant(String nom, String prenom, int age) {
+        super(nom, prenom, age);
+    }
+
+    public Enseignant(int idUtilisateur, String nom, String prenom, int age) {
+        super(idUtilisateur, nom, prenom, age);
+    }
+
+    public Enseignant(String nomUtilisateur, String email, String motDePasse) {
+        super(nomUtilisateur, email, motDePasse);
+    }
+
+    public Enseignant(String nom, String prenom, int age, String nomUtilisateur, String email, String motDePasse) {
+        super(nom, prenom, age, nomUtilisateur, email, motDePasse);
+    }
+
+    public Enseignant(int idUtilisateur, String nom, String prenom, int age, String nomUtilisateur, String email, String motDePasse) {
+        super(idUtilisateur, nom, prenom, age, nomUtilisateur, email, motDePasse);
+    }
 }
